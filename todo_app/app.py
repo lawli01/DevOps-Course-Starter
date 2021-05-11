21f1582a47e5b5d3558ed6f2babf8aa34593a660
Lawrence Li <lawrence_li2005@hotmail.com>
2021-05-11 19:20:36 +0100

    Display the list of saved todo items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,6 @@
+from todo_app.data.session_items import get_items
 from flask import Flask
-import flask
 from flask.templating import render_template
-
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
@@ -10,7 +9,7 @@ app.config.from_object(Config)
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('index.html', items=get_items())
 
 
 if __name__ == '__main__':
