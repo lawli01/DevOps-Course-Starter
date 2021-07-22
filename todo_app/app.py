c79e2d9fb636df5ba81610caada61746f4895701
Lawrence Li <lawrence_li2005@hotmail.com>
2021-07-22 21:45:25 +0100

    Move app creation to function

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,35 +4,41 @@ from todo_app.data.trello_items import get_items, add_item, delete_item, get_ite
 from flask import Flask, redirect, render_template, request
 from todo_app.flask_config import Config
 
-app = Flask(__name__)
-app.config.from_object(Config)
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object('todo_app.flask_config.Config')
+
+    @app.route('/')
+    def index():
+        items = get_items()
+        items_view_model = ItemsViewModel(items)
+        return render_template('index.html', view_model=items_view_model)
+
+    @app.route('/todos', methods=['POST'])
+    def add_todo():
+        title = request.form.get('title')
+        add_item(title)
+        return redirect('/')
+
+    @app.route('/todos/<id>', methods=['POST'])
+    def update_todo(id):
+        status = request.form.get('status')
+        item = get_item(id)
+        item.status = ItemStatus.COMPLETE if status == 'COMPLETE' else ItemStatus.NOT_STARTED
+        save_item(item)
+        return redirect('/')
+
+    @app.route('/todos/<id>/delete', methods=['POST'])
+    def remove_todo(id):
+        delete_item(id)
+        return redirect('/')
+
+    return app
 
 
-@app.route('/')
-def index():
-    items = get_items()
-    items_view_model = ItemsViewModel(items)
-    return render_template('index.html', view_model=items_view_model)
 
-@app.route('/todos', methods=['POST'])
-def add_todo():
-    title = request.form.get('title')
-    add_item(title)
-    return redirect('/')
-
-@app.route('/todos/<id>', methods=['POST'])
-def update_todo(id):
-    status = request.form.get('status')
-    item = get_item(id)
-    item.status = ItemStatus.COMPLETE if status == 'COMPLETE' else ItemStatus.NOT_STARTED
-    save_item(item)
-    return redirect('/')
-
-@app.route('/todos/<id>/delete', methods=['POST'])
-def remove_todo(id):
-    delete_item(id)
-    return redirect('/')
 
 
 if __name__ == '__main__':
-    app.run()
+    create_app().run()
