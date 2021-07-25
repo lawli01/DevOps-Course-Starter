4793436ae88782d28ee62baa249862a019a93ae8
Lawrence Li <lawrence_li2005@hotmail.com>
2021-07-25 19:03:51 +0100

    Add selenium e2e tests

--- tests_e2e/test_e2e.py
+++ tests_e2e/test_e2e.py
@@ -1,6 +1,8 @@
 import os
 
 from dotenv.main import find_dotenv, load_dotenv
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.firefox.webdriver import WebDriver
 from todo_app.app import create_app
 import pytest
 from selenium import webdriver
@@ -14,7 +16,7 @@ def app_with_temp_board():
     load_dotenv(file_path, override=True, verbose=True)
      # Create the new board & update the board id environment variable
     board_id = create_board()
-    os.environ['TRELLO_BOARD_ID'] = board_id
+    os.environ['TRELLO_API_TODO_BOARD_ID'] = board_id
     # construct the new application
     application = create_app()
     # start the app in its own thread.
@@ -35,6 +37,39 @@ def driver():
         yield driver
 
 
-def test_task_journey(driver, app_with_temp_board):
+def add_todo(driver: WebDriver, todo_name: str):
+    todo_input = driver.find_element_by_css_selector('[aria-label="todo input box"]')
+    todo_input.send_keys(todo_name)
+    add_todo_button = driver.find_element_by_css_selector('[aria-label="add todo"]')
+    add_todo_button.click()
+
+
+def click_todo_checkbox(driver: WebDriver, todo_name: str):
+    driver.find_element_by_css_selector(f'[aria-label="todo {todo_name} checkbox"]').click()
+
+
+def test_add_todo(driver: WebDriver, app_with_temp_board):
     driver.get('http://localhost:5000/')
+    add_todo(driver, todo_name="test")
+
+    new_todo = driver.find_element_by_css_selector('[aria-label="todo test label"]')
+    assert new_todo.text == 'test'
     assert driver.title == 'To-Do App'
+
+
+def test_can_mark_todo_as_complete(driver: WebDriver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+    click_todo_checkbox(driver, todo_name="test")
+
+    is_checked = driver.find_element_by_css_selector(f'[aria-label="todo test checkbox"]').is_selected()
+
+    assert is_checked == True
+
+
+def test_can_mark_todo_as_not_complete(driver: WebDriver, app_with_temp_board):
+    driver.get('http://localhost:5000/')
+    click_todo_checkbox(driver, todo_name="test")
+
+    is_checked = driver.find_element_by_css_selector(f'[aria-label="todo test checkbox"]').is_selected()
+
+    assert is_checked == False
